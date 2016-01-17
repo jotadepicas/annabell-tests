bff24d15f7c9165d5bea6c15b8cd11d96253400b
Jota De Picas <jotadepicas@gmail.com>
2016-01-17 12:46:16 -0300

    added tests for new methods in CommandUtils

--- src/ParseCommandTests.cpp
+++ src/ParseCommandTests.cpp
@@ -13,12 +13,27 @@ TEST(CommandUtilsTests, articleTest) {
 	EXPECT_EQ("shouldNotChange", CommandUtils::processArticle("shouldNotChange"));
 }
 
-TEST(CommandUtilsTests, stringUtilsTest) {
+TEST(CommandUtilsTests, startsWithCharTest) {
 	EXPECT_TRUE(CommandUtils::startsWith("abcdef", 'a'));
 	EXPECT_FALSE(CommandUtils::startsWith("abcdef", 'f'));
+}
 
+TEST(CommandUtilsTests, startsWithStringTest) {
+	EXPECT_TRUE(CommandUtils::startsWith("abcdef", "abc"));
+	EXPECT_TRUE(CommandUtils::startsWith("abcdef", "abcdef"));
+	EXPECT_FALSE(CommandUtils::startsWith("abcdef", "bcd"));
+}
+
+
+TEST(CommandUtilsTests, endsWithTest) {
 	EXPECT_TRUE(CommandUtils::endsWith("abcdef", 'f'));
 	EXPECT_FALSE(CommandUtils::endsWith("abcdef", 'a'));
+}
+
+TEST(CommandUtilsTests, removeTrailingTest) {
+	EXPECT_EQ("abcde", CommandUtils::removeTrailing("abcdef"));
+	EXPECT_EQ("abcdef", CommandUtils::removeTrailing("abcdef", 'x'));
+	EXPECT_EQ("abcdef", CommandUtils::removeTrailing("abcdef*", '*'));
 }
 
 int main(int argc, char **argv) {
